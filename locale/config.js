3cc5db3ba461c081f4b1d1c9e4e81ce246c2029b
Robert Korulczyk <robert@korulczyk.pl>
2022-08-30 18:21:00 +0200

    Update config.js

--- locale/config.js
+++ locale/config.js
@@ -1,8 +1,6 @@
 // source: https://unpkg.com/dayjs@v1.10.4/locale/xx.js
 // see: https://github.com/iamkun/dayjs/blob/v1.10.4/src/locale/xx.js
 
-// TODO: copy compiled translations from https://unpkg.com/dayjs@v1.10.4/locale/xx.js
-
 dayjs.locale({
     name: "uz-latn",
     weekdays: "Yakshanba_Dushanba_Seshanba_Chorshanba_Payshanba_Juma_Shanba".split("_"),
